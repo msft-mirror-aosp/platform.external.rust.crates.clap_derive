2557e8768162dd1bdcf3db7cbeda202fd72d6bb4
Andrew Walbran <qwandor@google.com>
2022-11-30 12:30:22 +0000

    Upgrade clap_derive to 4.0.21
    This project was upgraded with external_updater.
    Usage: tools/external_updater/updater.sh update rust/crates/clap_derive
    For more info, check https://cs.android.com/android/platform/superproject/+/master:tools/external_updater/README.md
    
    Test: TreeHugger
    Change-Id: If8dce056fa2766a1e7a681ee04676ccff006bc5d

--- src/utils/ty.rs
+++ src/utils/ty.rs
@@ -7,8 +7,9 @@ use syn::{
     PathSegment, Type, TypePath,
 };
 
-#[derive(Copy, Clone, PartialEq, Debug)]
+#[derive(Copy, Clone, PartialEq, Eq, Debug)]
 pub enum Ty {
+    Unit,
     Vec,
     Option,
     OptionOption,
@@ -21,7 +22,9 @@ impl Ty {
         use self::Ty::*;
         let t = |kind| Sp::new(kind, ty.span());
 
-        if is_generic_ty(ty, "Vec") {
+        if is_unit_ty(ty) {
+            t(Unit)
+        } else if is_generic_ty(ty, "Vec") {
             t(Vec)
         } else if let Some(subty) = subty_if_name(ty, "Option") {
             if is_generic_ty(subty, "Option") {
@@ -33,6 +36,17 @@ impl Ty {
             }
         } else {
             t(Other)
+        }
+    }
+
+    pub fn as_str(&self) -> &'static str {
+        match self {
+            Self::Unit => "()",
+            Self::Vec => "Vec<T>",
+            Self::Option => "Option<T>",
+            Self::OptionOption => "Option<Option<T>>",
+            Self::OptionVec => "Option<Vec<T>>",
+            Self::Other => "...other...",
         }
     }
 }
@@ -109,6 +123,14 @@ pub fn is_simple_ty(ty: &syn::Type, name: &str) -> bool {
 
 fn is_generic_ty(ty: &syn::Type, name: &str) -> bool {
     subty_if_name(ty, name).is_some()
+}
+
+fn is_unit_ty(ty: &syn::Type) -> bool {
+    if let syn::Type::Tuple(tuple) = ty {
+        tuple.elems.is_empty()
+    } else {
+        false
+    }
 }
 
 fn only_one<I, T>(mut iter: I) -> Option<T>
